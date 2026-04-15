9c6e4ff06d8b6a2fb981d6c935531526a4463f5e
San Benedeti <sanbenedeti@gmail.com>
2026-04-15 12:46:02 +0200

    new pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import Script from "next/script";
-import HeaderGate from "./HeaderGate";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,21 +14,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
-      <body className="bg-[#020817] text-white antialiased">
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18084183990"
-          strategy="afterInteractive"
-        />
-        <Script id="google-ads-tag" strategy="afterInteractive">
+      <head>
+        {/* META PIXEL */}
+        <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-18084183990');
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1459225639278250'); 
+            fbq('track', 'PageView');
           `}
         </Script>
+      </head>
 
-        <HeaderGate />
+      <body className="bg-[#020817] text-white antialiased">
         {children}
       </body>
     </html>
