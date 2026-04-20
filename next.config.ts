02355e36ed30d78a099a3227e0bde30eefe9f333
San Benedeti <sanbenedeti@gmail.com>
2026-04-20 11:31:53 +0200

    update CSP

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,38 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
-  connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://www.googletagmanager.com;
-  img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval'
+    https://www.googletagmanager.com
+    https://www.google-analytics.com
+    https://connect.facebook.net
+    https://www.googleadservices.com
+    https://googleads.g.doubleclick.net
+    https://www.google.com;
+  connect-src 'self'
+    https://www.google-analytics.com
+    https://region1.google-analytics.com
+    https://www.googletagmanager.com
+    https://www.googleadservices.com
+    https://googleads.g.doubleclick.net
+    https://www.google.com
+    https://stats.g.doubleclick.net
+    https://www.facebook.com
+    https://connect.facebook.net;
+  img-src 'self' data: blob:
+    https://www.google-analytics.com
+    https://region1.google-analytics.com
+    https://www.googletagmanager.com
+    https://www.googleadservices.com
+    https://googleads.g.doubleclick.net
+    https://www.google.com
+    https://stats.g.doubleclick.net
+    https://www.facebook.com
+    https://www.facebook.net;
   style-src 'self' 'unsafe-inline';
-  font-src 'self';
+  font-src 'self' data:;
+  frame-src 'self'
+    https://www.google.com
+    https://www.googletagmanager.com;
 `;
 
 const nextConfig: NextConfig = {
@@ -17,7 +44,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\n/g, ""),
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
           },
         ],
       },
